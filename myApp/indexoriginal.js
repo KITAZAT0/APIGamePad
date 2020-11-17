17954fab1646508c0f9c9879b578aef86f9bab78
KITAZAT0 <kevinbrandaobeauchard@gmail.com>
2020-11-17 16:22:52 +0100

    création de user et login fonctionnel

--- myApp/indexoriginal.js
+++ myApp/indexoriginal.js
@@ -9,7 +9,7 @@ console.log('todo list RESTful API server started on: ' + port); */
 const express = require('express'),
     app = express(),
     bodyParser = require('body-parser');
-const cors = require("cors");
+    const cors = require("cors");
 port = process.env.PORT || 3000; //port qui sera utilisé pour l'acces à l'api
 
 
@@ -17,15 +17,19 @@ app.listen(port);
 
 console.log('RESTful API server started on:' + port); // affichage dans le console log du port
 
-app.use(
-    cors(({
-        origin: ['http://localhost:3001'],
-        methods: ['GET', 'POST'],
-        credentials: true // enable set cookie
-    })),
-    bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(
+  cors({
+    origin: ["http://localhost:3001/"],
+    methods: ["GET", "POST"],
+    credentials: true, // enable set cookie
+  }),
+  bodyParser.urlencoded({ extended: true })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 const jwt = require('express-jwt');
 
 const jwksRsa = require('jwks-rsa');
