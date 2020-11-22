ed1750b821472b4e8cfddd150f351a8569d0bd9a
KITAZAT0 <kevinbrandaobeauchard@gmail.com>
2020-11-22 23:42:36 +0100

    debut nettoyage code

--- myApp/App/Controller/appController.js
+++ myApp/App/Controller/appController.js
@@ -1,56 +1,6 @@
 "use strict"; //fonctions qui serviront à nos routes
 // les fonctions ici utilsent les methodes task de appModel.js
 
-var Task = require("../model/appModel.js");
-
-exports.list_all_tasks = function (req, res) {
-  Task.getAllTask(function (err, task) {
-    console.log("controller");
-    if (err) res.send(err);
-    console.log("res", task);
-    res.send(task);
-  });
-};
-
-exports.create_a_task = function (req, res) {
-  //console.log(req.body);
-  var new_task = new Task(req.body);
-
-  //handlesnullerror
-  if (!new_task.task || !new_task.status) {
-    res
-      .status(400)
-      .send({ error: true, message: "Please provide task/status" });
-  } else {
-    Task.createTask(new_task, function (err, task) {
-      if (err) res.send(err);
-      res.json(task);
-    });
-  }
-};
-
-exports.read_a_task = function (req, res) {
-  Task.getTaskById(req.params.taskId, function (err, task) {
-    if (err) res.send(err);
-    res.json(task);
-  });
-};
-
-exports.update_a_task = function (req, res) {
-  Task.updateById(req.params.taskId, new Task(req.body), function (err, task) {
-    if (err) res.send(err);
-    res.json(task);
-  });
-};
-
-exports.delete_a_task = function (req, res) {
-  Task.remove(req.params.taskId, function (err, task) {
-    if (err) res.send(err);
-    res.json({ message: "Task success fully deleted" });
-  });
-};
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var User = require("../model/appModel.js");
 
--- myApp/index.js
+++ myApp/index.js
@@ -4,45 +4,43 @@
 app.listen(port);
 console.log('todo list RESTful API server started on: ' + port); */
 
-
-
-const express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
+const express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser");
 const cors = require("cors");
 port = process.env.PORT || 3000; //port qui sera utilisé pour l'acces à l'api
 
-
 app.listen(port);
 
-console.log('RESTful API server started on:' + port); // affichage dans le console log du port
+console.log("RESTful API server started on:" + port); // affichage dans le console log du port
 
 app.use(
-    cors(({
-        origin: ['http://localhost:3001'],
-        methods: ['GET', 'POST'],
-        credentials: true // enable set cookie
-    })),
-    bodyParser.urlencoded({ extended: true }));
+  cors({
+    origin: ["http://localhost:3001"],
+    methods: ["GET", "POST"],
+    credentials: true, // enable set cookie
+  }),
+  bodyParser.urlencoded({ extended: true })
+);
 app.use(bodyParser.json());
 
-const jwt = require('express-jwt');
+const jwt = require("express-jwt");
 
-const jwksRsa = require('jwks-rsa');
+const jwksRsa = require("jwks-rsa");
 
 const checkJwt = jwt({
-    secret: jwksRsa.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `https://apigamepad.eu.auth0.com/.well-known/jwks.json`
-    }),
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://apigamepad.eu.auth0.com/.well-known/jwks.json`,
+  }),
 
-    // Validate the audience and the issuer.
-    audience: 'https://GamePad-API',
-    issuer: `https://apigamepad.eu.auth0.com/`,
-    algorithms: ['RS256']
+  // Validate the audience and the issuer.
+  audience: "https://GamePad-API",
+  issuer: `https://apigamepad.eu.auth0.com/`,
+  algorithms: ["RS256"],
 });
 
-var routes = require('./App/Routes/appRoutes.js');//importing route
-routes(app.use(checkJwt));//register the route
+var routes = require("./App/Routes/appRoutes.js"); //importing route
+routes(app.use(checkJwt)); //register the route
